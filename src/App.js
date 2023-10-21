05116f8e192e7697f903713164c2e983f98568a5
unnath123 <unnathpgowda@gmail.com>
2023-10-21 14:09:00 +0530

    f4 contest w1 commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from '../src/components/Header'
+import style from './src/style'
+
+const App = () =>{
+    return (
+        <div>
+            <Header/>
+
+        </div>
+    )
+}
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Header = () =>{
+    return (
+        <div>
+            <div>
+                <h1>TYPECAT</h1>
+                <i class="fa-regular fa-keyboard" style={{color: "#e7eaee"}}></i>
+            </div>
+            <div>
+            <i class="fa-solid fa-user" style={{color: "#e5e7eb"}}></i>
+            </div>
+        </div>
+    )
+}
+
+export default Header
